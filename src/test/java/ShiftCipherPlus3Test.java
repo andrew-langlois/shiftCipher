13ce50a61ffa1c04e73b72734ef81d076db7db13
andrew-langlois <andrew@langlois.me.uk>
2021-06-03 20:32:50 +0100

    Tidy up directory structure

--- src/test/java/ShiftCipherPlus3Test.java
+++ src/test/java/ShiftCipherPlus3Test.java
@@ -1,7 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-public class JUnitShiftCipherTests {
+public class ShiftCipherPlus3Test {
 
   static final String HELLO = "HELLO";
   static final String KHOOR = "KHOOR";
